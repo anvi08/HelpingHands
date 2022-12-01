0d8f23bd7d9398e844c5314ad9f63fa8461e619b
abhishankar97 <abhishek.shankar@adqvest.com>
2022-12-01 14:36:01 -0500

    Testing Database and optimising UI

---
+++ HelpingHands/src/dbConnection/DbConnection.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dbConnection;
+
+/**
+ *
+ * @author abhis
+ */
+public class DbConnection {
+    
+
+    
+}
---
+++ HelpingHands/src/model/causes/CauseDirectory.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.causes;
+
+//import com.mysql.cj.xdevapi.Statement;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author abhis
+ */
+public class CauseDirectory {
+    private Cause cause;
+    Statement st;
+    
+    public CauseDirectory(Cause cause){
+        
+        this.cause = cause;
+        
+    }
+
+    public void addCause() throws SQLException{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/financialaiddb","root","@Fd2556b9dd1997");  
+            String sql = "INSERT INTO `cause`(`NGO_Org`, `Cause_Name`,`Cause_Desc`,`R_Category`,`Country`,`Status`) "
+                    + "VALUES ('" + cause.getNgoOrg() + "','" + cause.getCauseName() + "','" + cause.getCauseDesc() + "','" + cause.getRecCategory() + "','" + cause.getCountry() + "','" + '1' + "')";
+            st = con.createStatement();
+            st.execute(sql);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+//        fetch();
+    }    
+    
+}

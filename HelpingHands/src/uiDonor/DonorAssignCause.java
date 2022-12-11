66185258583891ba72c72e7cf61c3d70ee304bc3
abhishankar97 <abhishek.shankar@adqvest.com>
2022-12-11 17:44:12 -0500

    Debugged Code

--- HelpingHands/src/uiDonor/DonorAssignCause.java
+++ HelpingHands/src/uiDonor/DonorAssignCause.java
@@ -369,9 +369,13 @@ public class DonorAssignCause extends javax.swing.JPanel {
     private void btnAssignCauseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAssignCauseActionPerformed
         // TODO add your handling code here:
 
-        String cause = combobxCause.getSelectedItem().toString();
-        
+
+        if(txtAmount.getText().equals("")){
+            JOptionPane.showMessageDialog(this, "Please Enter a valid Amount");
+            return;
+        }
         ResultSet resultSet = donorDirectory.getdonor(loggedInUser.split("-")[0].trim());
+        String cause = combobxCause.getSelectedItem().toString();
         try {
             if (!resultSet.isBeforeFirst() ) {
                 System.out.println("No data");
@@ -427,6 +431,15 @@ public class DonorAssignCause extends javax.swing.JPanel {
 //                                txtRequirement.setVisible(false);
 //                                lblRequirement.setVisible(false);
                                 txtRequirement.setText("");
+                            }
+                            ArrayList<Receiver> allReceivers = null;
+                            String cause1 = combobxCause.getSelectedItem().toString();
+                            try {
+                                popDonorTable(donorDirectory.popDonorTable(cause1));
+                                allReceivers = serviceProviderDirectory.getService2(cause1);
+                                lblReqSub.setText(allReceivers.get(0).getFirstName().split(" ")[0].trim());
+                            } catch (SQLException ex) {
+                                Logger.getLogger(DonorAssignCause.class.getName()).log(Level.SEVERE, null, ex);
                             }                            
                             return;                            
                         } catch (ParseException ex) {
@@ -485,7 +498,8 @@ public class DonorAssignCause extends javax.swing.JPanel {
                             txtRequirement.setVisible(true);
                             lblRequirement.setVisible(true);
                             txtRequirement.setText(resultSet.getString("Requirement"));
-                            lblReqSub.setVisible(true);                                 
+                            lblReqSub.setVisible(true);
+                            lblReqSub.setText(resultSet.getString("Email"));
                         }else{
                             txtRequirement.setVisible(false);
                             lblRequirement.setVisible(false);
